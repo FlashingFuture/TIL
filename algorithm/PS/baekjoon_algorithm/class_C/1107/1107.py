be7ae7a4fec9e91208520140f9e193c18fb04796
FF <wjdrldud98@gmail.com>
2024-03-20 00:47:54 +0900

    0319 APS

--- algorithm/PS/baekjoon_algorithm/class_C/1107/1107.py
+++ algorithm/PS/baekjoon_algorithm/class_C/1107/1107.py
@@ -1,39 +1,27 @@
 import sys
 
 
-def backtrack(lev, num):
-    global min_diff
-    if min_diff < abs(num - N):
-        return
+def is_able(num):
+    temp = str(num)
+    for item in temp:
+        if item in B:
+            return 1000000
 
-    if lev == 6:
-        min_diff = abs(num - N)
-        return
-
-    for i in digit:
-        if lev == 5 and no_zero and i == 0: continue
-        path[lev] = i
-        backtrack(lev + 1, num + i * (10 ** (5 - lev)))
-        path[lev] = 0
+    return abs(num - N) + len(str(num))
 
 
-N = int(input())
-M = int(input())
+N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
 if M == 0:
     B = []
 else:
-    B = list(map(int, input().split()))
+    B = list(sys.stdin.readline().split())
 
-no_zero = False
-if 0 in B:
-    B.remove(0)
-    no_zero = True
-digit = [x for x in range(10) if x not in B]
-# 백트래킹으로 가장 가까운 수에 대해 접근
-# 그 후 +, -버튼만으로 목표 숫자에 접근
-# 그 후 그냥 +, -만 눌러서 간 값과 대소비교하여 결과 출력
-path = [0] * 6
-min_diff = sys.maxsize
-backtrack(0, 0)
-res = min(min_diff + len(str(N)), abs(N - 100))
+choose_min = 1000000
+for i in range(1000001):
+    cost = is_able(i)
+    if cost < choose_min:
+        choose_min = cost
+
+res = min(choose_min, abs(N - 100))
 print(res)
