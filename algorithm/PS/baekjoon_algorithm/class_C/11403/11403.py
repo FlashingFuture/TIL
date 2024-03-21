8cffeb04874414dcc3e99b2c2e59a15228dd6a88
FF <wjdrldud98@gmail.com>
2024-03-21 23:54:27 +0900

    0321 APS

--- algorithm/PS/baekjoon_algorithm/class_C/11403/11403.py
+++ algorithm/PS/baekjoon_algorithm/class_C/11403/11403.py
@@ -1,1 +1,24 @@
+from collections import deque
+
+
 N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+visited = [[0] * N for _ in range(N)]
+
+
+def bfs(start):
+    queue = deque([start])
+    while queue:
+        pos = queue.popleft()
+        for x in range(N):
+            if graph[pos][x] == 1 and not visited[start][x]:
+                queue.append(x)
+                visited[start][x] = 1
+
+
+for i in range(N):
+    bfs(i)
+
+
+for i in range(N):
+    print(*visited[i])
