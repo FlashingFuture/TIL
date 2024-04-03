5771894cd49e3099bd6a65e5ab928ae04225733c
FF <wjdrldud98@gmail.com>
2024-04-04 00:04:51 +0900

    0403 APS

--- algorithm/PS/baekjoon_algorithm/Star_and_NnM/Star/2448_star11.py
+++ algorithm/PS/baekjoon_algorithm/Star_and_NnM/Star/2448_star11.py
@@ -1,10 +1,10 @@
 triangle_delta = [
-    (1, 0), (0, 1), (0, -1), (-1, -2),
-    (-1, -1), (-1, 0), (-1, 1), (-1, 2)
+    (-1, 0), (0, 1), (0, -1), (1, -2),
+    (1, -1), (1, 0), (1, 1), (1, 2)
 ]
 
 N = int(input())
-chart = [[' '] * (2*N + 1) for _ in range(N + 1)]
+chart = [[' '] * (2*N - 1) for _ in range(N)]
 # 삼각형은 재귀적으로 2배씩 커지는 구조
 # N = 6일 때 삼각형 높이 12, 삼각형 밑변의 길이 12
 # 내부에 밑변 5짜리 삼각형 3개로 이루어진 구조
@@ -20,19 +20,16 @@ def triangle(y, x):
 
 def recur(y, x, n):
     if n == 1:
-        print(y, x)
         triangle(y, x)
         return
 
     n //= 2
     recur(y - 2 * n, x, n)
-    recur(y - n, x + 3 * n + 1, n)
-    recur(y - n, x - 3 * n - 1, n)
+    recur(y + n, x + 3 * n, n)
+    recur(y + n, x - 3 * n, n)
 
 
 P = N // 3
-recur(2 * P, 3 * P, P)
-for i in range(1, N + 1):
-    for j in range(1, 2*N + 1):
-        print(chart[i][j], end='')
-    print()
+recur(2 * P - 1, 3 * P - 1, P)
+for line in chart:
+    print(''.join(line))
