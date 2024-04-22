8f037248055be146677108f4c9f23e84678e5636
FF <wjdrldud98@gmail.com>
2024-04-22 22:24:45 +0900

    0422 APS

--- algorithm/PS/baekjoon_algorithm/class_E/1799/1799.py
+++ algorithm/PS/baekjoon_algorithm/class_E/1799/1799.py
@@ -12,74 +12,76 @@ plate = [list(map(int, input().split())) for _ in range(N)]
 # 인덱스의 합이 홀수인 격자, 짝수인 격자로 분할정복해 풀이하면
 # 2^50으로 바꿔 풀이할 수 있다
 # 각각 cannot 배열을 만들어 못 가는 자리를 확인하면서 백트래킹
-cannot_even = [[0] * N for _ in range(N)]
-cannot_odd = [[0] * N for _ in range(N)]
+cannot = [[0] * N for _ in range(N)]
 
 
-def make_cannot_even(y, x, disable):     # y, x에 비숍을 놓거나 뺄 때 갱신해주는 함수
+def make_cannot(y, x, disable):     # y, x에 비숍을 놓거나 뺄 때 갱신해주는 함수
     # 좌하향 대각선
     temp_y, temp_x = y, x
     while temp_y < N and temp_x >= 0:
-        cannot_even[temp_y][temp_x] = disable
+        cannot[temp_y][temp_x] += disable
         temp_y += 1
         temp_x -= 1
     # 우하향 대각선
     temp_y, temp_x = y + 1, x + 1
     while temp_y < N and temp_x < N:
-        cannot_even[temp_y][temp_x] = disable
+        cannot[temp_y][temp_x] += disable
         temp_y += 1
         temp_x += 1
 
 
-def make_cannot_odd(y, x, disable):     # y, x에 비숍을 놓거나 뺄 때 갱신해주는 함수
-    # 좌하향 대각선
-    temp_y, temp_x = y, x
-    while temp_y < N and temp_x >= 0:
-        cannot_odd[temp_y][temp_x] = disable
-        temp_y += 1
-        temp_x -= 1
-    # 우하향 대각선
-    temp_y, temp_x = y + 1, x + 1
-    while temp_y < N and temp_x < N:
-        cannot_odd[temp_y][temp_x] = disable
-        temp_y += 1
-        temp_x += 1
+def backtrack_even(y, x, num_of_bishop):    # (y + x)가 짝수인 판에 대한 백트래킹
+    if x >= N - 2:     # 맨 오른쪽 위치일 때
+        if y == N - 1:              # 마지막 줄일 때
+            global max_bishop_even
+            if plate[y][x] and not cannot[y][x]:
+                max_bishop_even = max(max_bishop_even, num_of_bishop + 1)
+            else:
+                max_bishop_even = max(max_bishop_even, num_of_bishop)
+            return
+        else:                       # 마지막 줄이 아닌 맨 오른쪽 위치일 때
+            if plate[y][x] and not cannot[y][x]:
+                make_cannot(y, x, 1)
+                backtrack_even(y + 1, (y + 1) % 2, num_of_bishop + 1)
+                make_cannot(y, x, -1)
+            backtrack_even(y + 1, (y + 1) % 2, num_of_bishop)
+    else:   # 맨 오른쪽이 아닐 때
+        if plate[y][x] and not cannot[y][x]:
+            make_cannot(y, x, 1)
+            backtrack_even(y, x + 2, num_of_bishop + 1)
+            make_cannot(y, x, -1)
+        backtrack_even(y, x + 2, num_of_bishop)
 
 
-def backtrack_even(y, x, num_of_bishop):
-    if y >= ((N - 1) // 2) * 2:
-        if x >= ((N - 1) // 2) * 2:
-            if plate[y][x] and not cannot_even[y][x]:
-
-
-
-
-def backtrack_odd(y, x, num_of_bishop):
-    if x >= (N // 2) * 2 - 2:
-        if y == N:  # 마지막 점일 때
+def backtrack_odd(y, x, num_of_bishop):     # (y + x)가 홀수인 판에 대한 백트래킹
+    if x >= N - 2:     # 맨 오른쪽 위치일 때
+        if y == N - 1:              # 마지막 줄일 때
             global max_bishop_odd
-            if plate[y][x] and not cannot_even[y][x]:
+            if plate[y][x] and not cannot[y][x]:
                 max_bishop_odd = max(max_bishop_odd, num_of_bishop + 1)
-            max_bishop_odd = max(max_bishop_odd, num_of_bishop)
-        elif
-
-        else:
-            if plate[y][x] and not cannot_odd[y][x]:
-                make_cannot_odd(y, x, 1)
-                backtrack_odd(y + 1, x % 2, num_of_bishop + 1)
-                make_cannot_odd(y, x, 0)
-            backtrack_odd(y + 1, x % 2, num_of_bishop)
-    else:
-        if plate[y][x] and not cannot_odd[y][x]:
-            make_cannot_odd(y, x, 1)
+            else:
+                max_bishop_odd = max(max_bishop_odd, num_of_bishop)
+            return
+        else:                       # 마지막 줄이 아닌 맨 오른쪽 위치일 때
+            if plate[y][x] and not cannot[y][x]:
+                make_cannot(y, x, 1)
+                backtrack_odd(y + 1, y % 2, num_of_bishop + 1)
+                make_cannot(y, x, -1)
+            backtrack_odd(y + 1, y % 2, num_of_bishop)
+    else:   # 맨 오른쪽이 아닐 때
+        if plate[y][x] and not cannot[y][x]:
+            make_cannot(y, x, 1)
             backtrack_odd(y, x + 2, num_of_bishop + 1)
-            make_cannot_odd(y, x, 0)
+            make_cannot(y, x, -1)
         backtrack_odd(y, x + 2, num_of_bishop)
 
 
-max_bishop_even = 0
-max_bishop_odd = 0
-backtrack_even(0, 0, 0)
-backtrack_odd(0, 1, 0)
-res = max_bishop_even + max_bishop_odd
-print(res)
+if N == 1:
+    print(plate[0][0])
+else:
+    max_bishop_even = 0
+    max_bishop_odd = 0
+    backtrack_even(0, 0, 0)
+    backtrack_odd(0, 1, 0)
+    res = max_bishop_even + max_bishop_odd
+    print(res)
