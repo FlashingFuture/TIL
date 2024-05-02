c4df6343c30f8ec1e380c34a0294bae9ab57a902
FF <wjdrldud98@gmail.com>
2024-05-02 12:08:16 +0900

    0502 vue

---
+++ prt.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p class="greeting">{{ msg }}</p>
+  </div>
+</template>
+
+<script setup>
+// setup function 
+import { ref } from 'vue'
+
+const msg = ref('Hello, Vue3!')
+</script>
+
+<style scoped>
+.greeting {
+  color: #888844;
+}
+</style>
